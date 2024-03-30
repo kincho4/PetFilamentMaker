3a9c5ad45bdd86b7a7077819b1235d0d81ea62e3
kincho4 <75807467+kincho4@users.noreply.github.com>
2024-03-30 18:46:05 +0000

    debug

--- thermistor.py
+++ thermistor.py
@@ -1,48 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-# Thermistor parameters
-R0 = 10000  # Resistance at 25 degrees Celsius
-beta = 3950  # Beta value of the thermistor
-
 # GPIO pin
 therm_pin = 12  # Use GPIO18 (pin 12) for thermistor input
 
-def get_temp():
+def get_analog_reading():
     # Setup GPIO
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(therm_pin, GPIO.IN)
 
     # Read analog value
-    analog_value = 0
-    for i in range(10):
-        analog_value += GPIO.input(therm_pin)
-        time.sleep(0.01)
-    analog_value /= 10.0
+    analog_value = GPIO.input(therm_pin)
 
-    print("Raw analog reading:", analog_value)  # Debug statement
-
-    # Check if analog reading is at maximum
-    if analog_value == 1023:
-        return None
-
-    # Calculate resistance of the thermistor
-    R = (analog_value * R0) / (1023 - analog_value)
-
-    # Calculate temperature in Celsius
-    T = 1 / (1 / 298.15 + 1 / beta * (1 / (273.15 + 25) - 1 / (R / 1000)))
-    T -= 273.15  # Convert Kelvin to Celsius
-
-    return T
+    return analog_value
 
 try:
     while True:
-        temp = get_temp()
-        if temp is not None:
-            print("Thermistor temperature:", temp)
-            print("----------------------")
-        else:
-            print("Analog reading at maximum, check wiring or thermistor")
+        analog_value = get_analog_reading()
+        print("Raw analog reading:", analog_value)
+
         time.sleep(1)  # Wait 1 second before next measurement
 
 except KeyboardInterrupt:
